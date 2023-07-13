a9b58b3f56e70aaa195867d671a624a324e3577a
Phuoc Huynh Thien <phuoc.huynh@brtchip.com>
2023-07-13 11:26:49 +0700

    sample grid stack

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,34 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import { GridStack, GridStackOptions } from 'gridstack'
+import 'gridstack/dist/gridstack.min.css'
+import 'gridstack/dist/gridstack-extra.min.css'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const subOptions = {
+      cellHeight: 50,
+      column: 'auto',
+      acceptWidgets: true,
+      margin: 5,
+      subGridDynamic: true,
+      itemClass: 'sub',
+    }
+    const options = {
+      animate: true,
+      cellHeight: 50,
+      margin: 5,
+      minRow: 1,
+      acceptWidgets: true,
+      subGridOpts: subOptions,
+      subGridDynamic: true,
+      float: true,
+    } as GridStackOptions
+    const grid = GridStack.init(options)
+    console.debug(`🚀${(new Date()).toISOString()} ~ file: App.tsx:28 ~ useEffect ~ grid:`, grid)
+    const grid2 = GridStack.init({
+      cellHeight: 50,
+      acceptWidgets: '.grid-stack-item.sub',
+      disableOneColumnMode: true,
+      margin: 1
+    }, '.grid-stack.nested2');
+    console.debug(`🚀${(new Date()).toISOString()} ~ file: App.tsx:34 ~ useEffect ~ grid2:`, grid2)
+    console.debug(
+      `🚀${new Date().toISOString()} ~ file: App.tsx:35 ~ useEffect ~ grid.save:`,
+      grid?.save(false, true)
+    )
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid-stack">
+      <div
+        className="grid-stack-item sub"
+        id={'uuid'}
+        key={'uuid'}
+        gs-w={3}
+        gs-min-w={2}
+        data-gs-auto-position="1"
+        gs-id='{"id":"ds23","data":{"as":12,"as4":{"ds":12,"fdf3":344}}}'
+      >
+        <div className="grid-stack-item-content">
+          <button onClick={() => alert('asdasbcubasc absd ')}>
+            as das
+          </button>
+        </div>
       </div>
-      <h1>Vite + React haha</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div
+        className="grid-stack-item sub"
+        id={'uuid1'}
+        key={'uuid1'}
+        gs-w={3}
+        gs-min-w={2}
+        data-gs-auto-position="5"
+      >
+        <div className="grid-stack-item-content">
+          <button onClick={() => alert('asdasbcubasc absd ')}>
+            as das asd as
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className="grid-stack-item grid-stack-draggable-item" gs-x="5" gs-y="0" gs-w="3" gs-h="4">
+        <div className="grid-stack-item-content">
+          <button onClick={() => alert('asdasbcubasc absd 7')}>
+            nested
+          </button>
+          <div className="grid-stack nested2">
+            <div className="grid-stack-item sub" gs-x="0" gs-y="0" gs-w="3">
+              <div className="grid-stack-item-content" >
+                <button onClick={() => alert('asdasbcubasc absd 7')} >
+                  7
+                </button>
+              </div>
+            </div>
+            <div className="grid-stack-item sub" gs-x="5" gs-y="5" gs-w="3" gs-h="3">
+              <div className="grid-stack-item-content">
+                <button onClick={() => alert('asdasbcubasc absd 7')} >
+                  8
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
